d336b4a4551e8ad9fff575b13bc6bb4fffef1a53
Roshan Mathews <rmathews@gmail.com>
2012-09-23 21:04:01 +0530

    Modified Conditional: added a ifC to the core language.

--- Conditional-pt.rkt
+++ Conditional-pt.rkt
@@ -5,13 +5,17 @@
 (define-type ArithC
   [numC (n : number)]
   [plusC (l : ArithC) (r : ArithC)]
-  [multC (l : ArithC) (r : ArithC)])
+  [multC (l : ArithC) (r : ArithC)]
+  [ifC (predicate : ArithC) (true : ArithC) (false : ArithC)])
 
 (define (interp [a : ArithC]) : number
   (type-case ArithC a
     [numC (n) n]
     [plusC (l r) (+ (interp l) (interp r))]
-    [multC (l r) (* (interp l) (interp r))]))
+    [multC (l r) (* (interp l) (interp r))]
+    [ifC (p t f) (if (zero? (interp p))
+                     (interp t)
+                     (interp f))]))
 
 ;; Surface Language
 
@@ -20,7 +24,7 @@
   [plusS (l : ArithS) (r : ArithS)]
   [bminusS (l : ArithS) (r : ArithS)]
   [multS (l : ArithS) (r : ArithS)]
-  [ifS (predicate : number) (true : ArithS) (false : ArithS)])
+  [ifS (predicate : ArithS) (true : ArithS) (false : ArithS)])
 
 (define (parse s) : ArithS
   (cond 
@@ -31,10 +35,11 @@
          [(+) (plusS (parse (second sl)) (parse (third sl)))]
          [(*) (multS (parse (second sl)) (parse (third sl)))]
          [(-) (bminusS (parse (second sl)) (parse (third sl)))]
-         [(if) (ifS (s-exp->number (second sl))
-                    (parse (third sl)) 
-                    (parse (fourth sl)))]))]
-    [else (error 'parse "invalid list input")]))
+         [(if0) (ifS (parse (second sl))
+                     (parse (third sl)) 
+                     (parse (fourth sl)))]
+         [else (error 'parse "invalid input")]))]
+    [else (error 'parse "invalid input")]))
 
 (define (desugar [as : ArithS]) : ArithC
   (type-case ArithS as
@@ -44,6 +49,6 @@
                           (multC (numC -1) 
                                  (desugar r)))]
     [multS (l r) (multC (desugar l) (desugar r))]
-    [ifS (p t f) (if (not (= p 0)) 
-                     (desugar t)
-                     (desugar f))]))
+    [ifS (p t f) (ifC (desugar p)
+                      (desugar t)
+                      (desugar f))]))
